8e848aa3d4bbd1bbe3c9d5071155a4025fd71ff0
Ashik Eqbal <hello@itsashik.info>
2023-12-21 05:45:53 +0530

    Refactor About Us page component

--- app/(root)/about-us/page.tsx
+++ app/(root)/about-us/page.tsx
@@ -1,45 +1,12 @@
-import { Separator } from '@/components/ui/separator';
-import { client } from '@/sanity/lib/client'
-import PortableText from 'react-portable-text';
+import React from 'react';
 
-type AboutUs = {
-  title: string,
-  content: string
-}
-
-
-export async function AboutUs() {
-  const aboutus = await client.fetch(`*[_type == "aboutus"]{
-    _id,
-    title,
-    content,
-  }`);
+const AboutUs: React.FC = () => {
   return (
-    <article>
-      <div
-        className='p-3 max-w-6xl mx-auto'
-      >
-        <div
-          className='flex flex-col items-center justify-center p-1'
-        >
-          <h1
-            className='text-4xl font-bold text-center flex flex-col items-center justify-center text-gray-800 md:text-4xl'
-          >
-            {aboutus[0]?.title}
-            <Separator
-            className='w-4/5 h-1 mt-1 bg-gray-800 rounded-full'
-            />
-          </h1>
-        </div>
-        <PortableText
-        content={aboutus[0]?.content}
-        className=' text-gray-800 text-justify mt-2'
-        />
-      </div>
-    </article>
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About Us page.</p>
+    </div>
+  );
+};
 
-    );
-}
-
-
-export default AboutUs
+export default AboutUs;
